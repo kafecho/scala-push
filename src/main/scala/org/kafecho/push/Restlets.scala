7a68d5aa2df88bca79cbb785562e29db47a18b73
Guillaume Belrose <kafecho@gmail.com>
2009-11-29 15:43:37 +0000

    Better javadoc, using md5 hashes instead of JVM hashCodes.

--- src/main/scala/org/kafecho/push/Restlets.scala
+++ src/main/scala/org/kafecho/push/Restlets.scala
@@ -36,13 +36,13 @@ class AdminRestlet (val subscriber : Subscriber ) extends Default with Logging w
 		
 		if (request.getEntity.getMediaType != MediaType.APPLICATION_WWW_FORM){
 		  log.error("Invalid MIME type: " + request.getEntity.getMediaType)
-		  response.setStatus(Status.CLIENT_ERROR_METHOD_NOT_ALLOWED)
+		  response.setStatus(Status.CLIENT_ERROR_UNSUPPORTED_MEDIA_TYPE)
 		  return
 		}
 		val form = request.getEntityAsForm
 		val feedURL = form.getFirstValue("feedURL")
 		if (feedURL != null){
-			subscriber.subUnsub(feedURL , true)
+			subscriber.subUnsub(feedURL,true)
 			response.setEntity("Creating subscription for feed: " + feedURL,MediaType.TEXT_PLAIN)
 			response.setStatus(Status.SUCCESS_ACCEPTED)
 		}
@@ -52,11 +52,11 @@ class AdminRestlet (val subscriber : Subscriber ) extends Default with Logging w
 		val id = request.getAttributes.get("ID")
 		if (id == null) response.setStatus(Status.CLIENT_ERROR_METHOD_NOT_ALLOWED)
 		else{
-		  val found = subscriber.activeSubscriptions.find( _.id.toString == id)
+		  val found = subscriber.activeSubscriptions.find( _.id == id)
 		  found match{
-		    case Some(subscription) =>
-		    	subscriber.subUnsub(subscription.feedURL,subscription.topicURL,subscription.hubURL,false)
-       			response.setEntity("Removing subscription for feed: " +  subscription.feedURL, MediaType.TEXT_PLAIN)
+		    case Some(feed) =>
+		    	subscriber.subUnsub(feed,false)
+       			response.setEntity("Removing subscription for feed: " +  feed, MediaType.TEXT_PLAIN)
        			response.setStatus(Status.SUCCESS_ACCEPTED)
 		    case None => response.setStatus(Status.CLIENT_ERROR_NOT_FOUND)	
 		  }
@@ -65,9 +65,9 @@ class AdminRestlet (val subscriber : Subscriber ) extends Default with Logging w
 	def doGET(request : Request, response : Response){
 		val id = request.getAttributes.get("ID")
 		if (id != null){
-		  val found = subscriber.activeSubscriptions.find( _.id.toString == id)
+		  val found = subscriber.activeSubscriptions.find( _.id == id)
 		  found match{
-		    case Some(subscription) => response.setEntity(xmlPrinter.format(subscription.toXML), MediaType.TEXT_XML)
+		    case Some(feed) => response.setEntity(xmlPrinter.format(feed.toXML), MediaType.TEXT_XML)
 		    case None => response.setStatus(Status.CLIENT_ERROR_NOT_FOUND)	
 		  }
 		}
@@ -82,7 +82,6 @@ class AdminRestlet (val subscriber : Subscriber ) extends Default with Logging w
 class PuSHRestlet (val subscriber : Subscriber) extends Default with Logging with GET with POST{
 	/**
 	 * Handle subscribe / unsubscribe challenges from a hub.
-	 * TODO: add checks to ensure subscriber does not blindly accept challenges from hub.	 
 	 */
 	def doGET(request: Request, response:Response){
 	  	val params 	= request.getResourceRef.getQueryAsForm
@@ -91,45 +90,32 @@ class PuSHRestlet (val subscriber : Subscriber) extends Default with Logging wit
 	  	val lease_seconds = params.getFirstValue(Constants.lease_seconds).toInt
 	  	
 	  	log.info("Received hub challenge:\n-Mode: " + mode + "\n-Topic: " + topic + "\n-Lease: " + lease_seconds + " seconds")
-		response.setEntity(params.getFirstValue(Constants.challenge),MediaType.TEXT_PLAIN)
-	
+
+	  	// TODO: add appropriate checks to ensure the subscriber does not blindly accept challenges from hub.	 
+	  	response.setEntity(params.getFirstValue(Constants.challenge),MediaType.TEXT_PLAIN)
+
 		mode match{
-		  case Constants.subscribe 	 => subscriber.added(topic) 
-		  case Constants.unsubscribe => subscriber.removed(topic)
+		  case Constants.subscribe 	 => subscriber.subscribed(topic) 
+		  case Constants.unsubscribe => subscriber.unsubscribed(topic)
 		}
   
   		log.info("Acknowledged hub challenge.")
 	}
 
 	/*
-	 * Handle content notifications from a hub.
+	 * Handle content notification from a hub.
 	 */
 	def  doPOST(request: Request,response:Response){
+	    log.info("Incoming POST request: " + request.getResourceRef)
 		val entity = request.getEntity
 		if (entity.getMediaType == MediaType.APPLICATION_ATOM_XML){
 			response.setStatus (Status.SUCCESS_OK)
 			val atom = XML.load(entity.getStream)
-			processPost(atom)
+			subscriber.contentPublished(atom)
 		}
 		else{
-			response.setStatus (Status.CLIENT_ERROR_UNSUPPORTED_MEDIA_TYPE)
    			log.error("Received hub notification containing unsupported payload of type " + entity.getMediaType + "\n" + entity.getText)
-		}
-	}
- 
-	/*
-	* Extract some useful info from a post for display on the screen.	 	
-	*/
-	def processPost(atom : Elem){
-		val Some(topicNode) = (atom\"link").find( _\"@rel" == "self")
-		println (RFC3339.parse((atom\"updated").text) + " --- Update from: " + (atom\"title").text )
-		println ("Topic URL: " + topicNode\"@href" + "\n")
-  
-		(atom\"entry").foreach{ e=>
-			val link = (e\"link").find( _\"@rel" == "alternate").get\"@href"
-			val entryTitle = (e\"title").text
-			val entryPublished = (e\"published").text
-			println (entryTitle + " (published " + RFC3339.parse(entryPublished) + ")\n" + link + "\n")
+			response.setStatus (Status.CLIENT_ERROR_UNSUPPORTED_MEDIA_TYPE)
 		}
 	}
 }
